8f29080d0e85a7c7e3c8ebb135d9a9cec104216f
abdes96 <abdeslam.boutaarourt@student.ehb.be>
2024-01-02 19:57:37 +0100

    backup folder

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const knex = require('knex')({
+    client: 'pg',
+    connection: {        
+        host : '127.0.0.1',
+        user : 'your_database_user',
+        password : 'your_database_password',
+        database : 'your_database_name'
+    }
+});
+
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.send('Hello World!');
+    knex.select('*').from('your_table_name')
+        .then(data => res.send(data))
+        .catch(err => res.status(500).send('An error occurred'));
+
 });
 
 app.listen(port, () => {
