53017d7dce5ce0c1ff76274495b9a4090d1fe36a
abdes96 <abdeslam.boutaarourt@student.ehb.be>
2024-01-04 21:35:49 +0100

    api service can connect to PostgreSQL database in the store service (named: postgres)

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -1,25 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
+const knex = require("knex")(require("./knexfile.js").development);
 
-const knex = require('knex')({
-    client: 'pg',
-    connection: {        
-        host : '127.0.0.1',
-        user : 'your_database_user',
-        password : 'your_database_password',
-        database : 'your_database_name'
-    }
-});
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    knex.select('*').from('your_table_name')
-        .then(data => res.send(data))
-        .catch(err => res.status(500).send('An error occurred'));
-
+app.get("/", (req, res) => {
+  knex
+    .select("*")
+    .from("users")
+    .then((data) => res.send(data))
+    .catch((err) => res.status(500).send("An error occurred"));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
